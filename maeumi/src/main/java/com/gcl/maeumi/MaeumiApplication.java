76862937572a7f4c0a67a530a68f5f58c8e42ef8
sforseohn <sforseohn@ewhain.net>
2024-10-01 16:51:26 +0900

    feat: initial project settings

---
+++ maeumi/src/main/java/com/gcl/maeumi/MaeumiApplication.java
@@ -0,0 +1,13 @@
+package com.gcl.maeumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaeumiApplication.class, args);
+	}
+
+}
---
+++ maeumi/src/test/java/com/gcl/maeumi/MaeumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcl.maeumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
