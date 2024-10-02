03fc05720bbac29e51253257462eef99776192bf
sforseohn <sforseohn@ewhain.net>
2024-10-02 22:40:34 +0900

    feat: add common api response

--- maeumi/src/main/java/com/gcl/maeumi/member/controller/MemberController.java
+++ maeumi/src/main/java/com/gcl/maeumi/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.gcl.maeumi.member.controller;
 
 import com.gcl.maeumi.member.dto.MemberDto;
 import com.gcl.maeumi.member.service.MemberService;
+import com.gcl.maeumi.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 @RestController
@@ -12,12 +13,11 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody MemberDto.SignupRequestDto request) {
+    public ApiResponse<String> signup(@RequestBody MemberDto.SignupRequestDto request) {
         if (memberService.join(request)) {
-            return request.toString();
-        }
-        else {
-            return "회원가입에 실패했습니다.";
+            return ApiResponse.createSuccess("회원가입에 성공했습니다.");
+        } else {
+            return ApiResponse.createError("회원가입에 실패했습니다.");
         }
     }
 }
