0562a10818337560055baf6687d546c4a0955021
sforseohn <sforseohn@ewhain.net>
2024-10-30 00:05:43 +0900

    feat: add nginx, docker configs

--- maeumi/src/main/java/com/gcl/maeumi/MaeumiApplication.java
+++ maeumi/src/main/java/com/gcl/maeumi/MaeumiApplication.java
@@ -3,6 +3,8 @@ package com.gcl.maeumi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaAuditing
 @SpringBootApplication
