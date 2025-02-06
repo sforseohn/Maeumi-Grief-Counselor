f771009655f101ca7fe99631225710600f60efaf
sforseohn <sforseohn@ewha.ac.kr>
2025-02-06 17:34:53 +0900

    chore: rename enum value (text -> descriptive)

--- maeumi/src/main/java/com/gcl/maeumi/chat/AnswerType.java
+++ maeumi/src/main/java/com/gcl/maeumi/chat/AnswerType.java
@@ -1,7 +1,7 @@
-package com.gcl.maeumi.answer;
+package com.gcl.maeumi.chat;
 
 public enum AnswerType {
     MULTIPLE_CHOICE, // 객관식 응답
-    TEXT, // 서술형 응답
+    DESCRIPTIVE, // 서술형 응답
     SKIP; // 사용자 응답 없이 다음 질문으로 넘어감
 }
